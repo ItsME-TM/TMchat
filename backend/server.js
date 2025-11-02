9e51ee8409509fd8aaa9255078ac4ccfac4c78ee
ItsME-TM <thushanmalaka123@gmail.com>
2025-11-02 22:48:49 +0530

    Create simple api and installed nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,17 @@
-const express = require('express');
+import express from "express";
 
 const app = express();
+
+app.get("/api/auth/signup", (req, res) => {
+    res.send("Signup route");
+})
+
+app.get("/api/auth/login", (req, res) => {
+    res.send("Login route");
+})
+
+app.get("/api/auth/logout", (req, res) => {
+    res.send("Logout route");
+})
+
 app.listen(3000, () => console.log("Server running on port 3000"));
