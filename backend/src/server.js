33a59d87810be256b06f16358ff5ed78412f465a
ItsME-TM <thushanmalaka123@gmail.com>
2025-11-02 23:14:25 +0530

    Create separate module for message routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const app = express();
@@ -8,5 +9,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => console.log("Server running on port: " + PORT));
