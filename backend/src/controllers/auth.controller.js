d3f400b4f2876f7467016b88f9d52309f7657349
ItsME-TM <thushanmalaka123@gmail.com>
2025-11-05 02:27:22 +0530

    Fix some issues and test the signup api using postman

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,60 +1,62 @@
 import { profile } from "console";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/utils.js";
 
 export const signup = async (req, res) => {
-    const {fullName, email, password} = req.body;
-    try{
-        if(!fullName || !email || !password){
-            return res.status(400).json(
-                {message: "Please provide all required fields: fullName, email, password"}
-            );
-        }
-        if (password.length < 6){
-            return res.status(400).json(
-                {message: "Password must be at least 6 characters long"}
-            );
-        }
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if(!emailRegex.test(email)){
-            return res.status(400).json(
-                {message: "Please provide a valid email address"}
-            );
-        }
-
-        const user = await User.findOne({email});
-        if(user){
-            return res.status(400).json(
-                {message: "User with this email already exists"}
-            );
-        }
-        const salt = await bcrypt.getSalt(10);
-        const hashPassword = await bcrypt.hash(password, salt);
-
-        const newUser = new User({
-            fullName,
-            email,
-            password: hashPassword
+  const { fullName, email, password } = req.body;
+  try {
+    if (!fullName || !email || !password) {
+      return res
+        .status(400)
+        .json({
+          message:
+            "Please provide all required fields: fullName, email, password",
         });
-
-        if(newUser){
-            //we send the token thats why we pass the res object
-            generateToken(newUser._id, res);
-            await newUser.save();
-
-            res.status(201).json({
-                _id: newUser._id,
-                fullName: newUser.fullName,
-                email: newUser.email,
-                profilePic: newUser.profilePic,
-            });
-        }else{
-            res.status(400).json({message: "Error creating user"});
-        }
-
-    }catch(error){
-        res.status(500).json({message: "Server error"});
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res
+        .status(400)
+        .json({ message: "Please provide a valid email address" });
     }
 
-    res.send("Signup controller");
-} 
+    const user = await User.findOne({ email });
+    if (user) {
+      return res
+        .status(400)
+        .json({ message: "User with this email already exists" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashPassword,
+    });
+
+    if (newUser) {
+      //we send the token thats why we pass the res object
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ message: "Error creating user" });
+    }
+  } catch (error) {
+    console.error("Signup error:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
