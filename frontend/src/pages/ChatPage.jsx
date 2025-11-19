f7211b9964f7705dda733db2e080336bc113eab2
ItsME-TM <thushanmalaka123@gmail.com>
2025-11-19 10:44:24 +0530

    Implemented the Loging Frontend

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { useAuthStore } from '../store/useAuthStore';
 
 function ChatPage() {
+  const {logout } =  useAuthStore();
+
   return (
-    <div>
-      ChatPage
+    <div className="z-10">
+      Chat
+      <button onClick={logout}>Logout</button>
     </div>
   )
 }
 
-export default ChatPage
+export default ChatPage;
