5f0738ad98ef2676a7241b30defd13236e6a2477
ItsME-TM <thushanmalaka123@gmail.com>
2025-11-20 11:09:25 +0530

    Setup the Chatpage layout

---
+++ frontend/src/components/NoConversationPlaceHolder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NoConversationPlaceHolder() {
+  return (
+    <div>
+      NoConversationPlaceHolder
+    </div>
+  )
+}
+
+export default NoConversationPlaceHolder
